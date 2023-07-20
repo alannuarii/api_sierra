c94ca4e7b1d1f4ce8eec6433df670ad2f33e95ee
sierrawebid <sierrawebid@gmail.com>
2023-07-21 07:53:09 +0800

    Update

--- app/controller/weather.py
+++ app/controller/weather.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from db import connection
+
 
 class Weather:
     url = "https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-SulawesiUtara.xml"
-    response = requests.get(url,verify=True)
+    response = requests.get(url, verify=True)
     r = response.text
-    sangihe = bs(r,"xml")
-
+    sangihe = bs(r, "xml")
 
     def get_weather(self, hour):
         cuaca = self.sangihe.find(id="501536").find(id="weather")
@@ -14,20 +15,17 @@ class Weather:
         result = h12
         return result
 
-    
     def get_temperature(self, hour):
         temperature = self.sangihe.find(id="501536").find(id="t")
         h12 = temperature.find(h=hour).value.string
         result = h12
         return result
-    
 
     def get_humidity(self, hour):
         humidity = self.sangihe.find(id="501536").find(id="hu")
         h12 = humidity.find(h=hour).value.string
         result = h12
         return result
-    
 
     def get_wind(self, hour):
         humidity = self.sangihe.find(id="501536").find(id="ws")
@@ -35,3 +33,9 @@ class Weather:
         result = h12
         return result
 
+    def insert_weather(self):
+        tanggal = "2023-07-21"
+        kode = int(self.get_weather("12"))
+        query = f"INSERT INTO weather (tanggal, kode) VALUES (%s, %s)"
+        value = [tanggal, kode]
+        connection(query, "insert", value)
---
+++ schedule.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# import sys
+# sys.path.append('/home/alanwebi/sierrapy/app')
+
+
+from app.controller.weather import Weather
+
+def post_weather():
+    print('APA')
+
+if __name__ == "__main__":
+    post_weather()
