86b0a99a814332664df62ae949d16e4f5e144db4
sierrawebid <sierrawebid@gmail.com>
2023-07-25 11:42:36 +0800

    Update

---
+++ app/controller/rekap.py
@@ -0,0 +1,8 @@
+from app.controller.rom import ROM
+from app.controller.weather import Weather
+from app.controller.irradiance import Irradiance
+from app.controller.prediction import prediction, get_arr_irradiance
+
+def rekap_data(bulan):
+    pass
+    
